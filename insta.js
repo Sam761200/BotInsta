ebc9843bd9a5bab0422581e5b23c6efd34616667
Samuel Saunier <samuel.saunier761200@gmail.com>
2022-02-03 15:18:29 +0100

    bot insta complet

---
+++ insta.js
@@ -0,0 +1,30 @@
+const puppeteer = require("puppeteer");
+require("dotenv").config();
+
+const url = "https://instagram.com";
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(url, { waitUntil: "networkidle2" });
+
+  // cookie
+  await page.click('._1XyCr > button');
+
+  // login
+  await page.type("[name=username]", "samuel_saunier@yahoo.fr", { delay: 100 });
+  await page.type("[name=password]", process.env.INSTA_PASS, { delay: 100 });
+
+  await page.click("button[type=submit]");
+
+  // auto connect
+  await page.waitForSelector(".cmbtv > button", { visible: true });
+  await page.click(".cmbtv > button");
+
+  // notif
+  await page.waitForSelector(".mt3GC > button", { visible: true });
+  await page.click(".mt3GC > button");
+
+  debugger;
+  // await browser.close();
+})();
